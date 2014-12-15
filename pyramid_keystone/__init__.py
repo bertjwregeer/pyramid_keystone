9bad69aa4d72d89b7f50eeff5033777d85f2caa0
Bert JW Regeer <bertjw@regeer.org>
2014-12-14 23:45:43 -0700

    Add some default settings, and a parser

--- pyramid_keystone/__init__.py
+++ pyramid_keystone/__init__.py
@@ -1,8 +1,23 @@
 
+default_settings = [
+        ('auth_url', str, 'http://localhost:5000/v3'),
+        ('region', str, 'RegionOne'),
+        ('user_domain_name', str, 'Default'),
+        ('cacert', str, ''),
+]
 
+def parse_settings(settings):
+    parsed = {}
 
+    def populate(name, convert, default):
+        sname = '%s%s' % ('keystone.', name)
+        value = convert(settings.get(sname, default))
+        parsed[sname] = value
 
+    for name, convert, default in default_settings:
+        populate(name, convert, default)
 
+    return parsed
 
 def includeme(config):
     """ Set up standard configurator registrations.  Use via:
