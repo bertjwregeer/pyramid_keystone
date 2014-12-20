f01eb0a4bcbaabfd968c10b317bc261f6957d72a
Bert JW Regeer <bertjw@regeer.org>
2014-12-20 01:51:42 -0700

    Add method to get a keystone session
    This uses either password authentication or token authentication. It
    then fetches an unscoped token.
    
    This adds the keystone token to the session, this is then later used to
    re-authenticate without needing the username/password, so over the
    lifetime of the user being logged in, they use the same token.

--- pyramid_keystone/keystone.py
+++ pyramid_keystone/keystone.py
@@ -1,3 +1,10 @@
+import logging
+log = logging.getLogger(__name__)
+
+from keystoneclient import session as kc_session
+from keystoneclient.auth.identity import v3 as kc_v3
+
+from keystoneclient.openstack.common.apiclient import exceptions as kc_exceptions
 
 from .settings import from_settings
 
@@ -7,10 +14,49 @@ def request_keystone(request):
 class Keystone(object):
     user_id = None
     username = None
+    _authed = False
+    _session = None
 
     def __init__(self, request):
         self.request = request
         self.settings = from_settings(request.registry.settings)
         self.settings = { k: v for (k, v) in self.settings.items() if v }
 
+    def _get_unscoped_session(self, token=None, username=None, password=None):
+        try:
+            auth_password = kc_v3.PasswordMethod(
+                    user_domain_name=self.settings['user_domain_name'],
+                    username=username,
+                    password=password
+                    )
+
+            auth_token = kc_v3.TokenMethod(
+                    token=token
+                    )
+
+            auth_methods = []
+
+            if token is not None:
+                auth_methods.append(auth_token)
+
+            if username is not None and password is not None:
+                auth_methods.append(auth_password)
+
+            auth = kc_v3.Auth(auth_url=self.settings['auth_url'], auth_methods=auth_methods)
+
+            sess = kc_session.Session(auth=auth)
+
+            log.debug('Fetching unscoped token')
+            unscoped_token = sess.get_token()
+            log.debug('Fetched unscoped token: {}'.format(unscoped_token))
+
+            if unscoped_token:
+                self._authed = True
+                self._session = sess
+                self.request.session['keystone_token'] = unscoped_token
+            else:
+                if 'keystone_token' in self.request.session:
+                    del self.request.session['keystone_token']
+        except (kc_exceptions.Unauthorized, kc_exceptions.AuthorizationFailure) as e:
+            return
 
