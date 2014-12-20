31e435a7d72d19c289882f10accdbcec1c36c51a
Bert JW Regeer <bertjw@regeer.org>
2014-12-20 01:49:41 -0700

    We return a keystone object that contains a request

--- pyramid_keystone/keystone.py
+++ pyramid_keystone/keystone.py
@@ -1,10 +1,17 @@
 from keystoneclient import client
 
+from .settings import from_settings
+
 def request_keystone(request):
-    return Keystone()
+    return Keystone(request)
 
 class Keystone(object):
     user_id = None
     username = None
 
-    pass
+    def __init__(self, request):
+        self.request = request
+        self.settings = from_settings(request.registry.settings)
+        self.settings = { k: v for (k, v) in self.settings.items() if v }
+
+
